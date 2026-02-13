bb9f4eee10001a6148d34634d6e2dac7ea49f23a
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2026-02-13 11:05:43 +0100

    feat: add fonts and icons

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import icons from "unplugin-icons/vite";
 import unocss from "unocss/astro";
 
 export default defineConfig({
   integrations: [unocss()],
+  vite: { plugins: [icons({ compiler: "astro" })] },
 });
