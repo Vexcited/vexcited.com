840a25746d4aa8aa34416634520c488ac6a80e4b
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2026-02-13 12:05:30 +0100

    feat: a bit more responsive on cards

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig, presetWind4 } from "unocss";
+import { defineConfig, presetWind4, transformerVariantGroup } from "unocss";
 
 export default defineConfig({
   presets: [presetWind4()],
+  transformers: [transformerVariantGroup()],
 });
